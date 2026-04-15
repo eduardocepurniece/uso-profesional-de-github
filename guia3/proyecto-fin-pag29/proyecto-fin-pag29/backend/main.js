24dd81be10c719bfe5d27cf30018eddba3d0359e
gmfm <gmfm282@gmail.com>
2026-04-15 03:38:17 -0400

    commit 3: agregue event listener a boton & cree funcion cargarClima con funcionalidad basica

--- guia3/proyecto-fin-pag29/proyecto-fin-pag29/backend/main.js
+++ guia3/proyecto-fin-pag29/proyecto-fin-pag29/backend/main.js
@@ -1,2 +1,21 @@
-//commit 2: agregue constante boton referenciando boton del DOM.
+//commit 3: agregue event listener a boton & cree funcion cargarClima con funcionalidad basica
+
 const boton = document.getElementById("cargarTmp");
+
+boton.addEventListener("click", () => {
+    cargarClima();
+})
+
+async function cargarClima() {
+    const temperatura = 30;
+
+    document.getElementById("temp").textContent = temperatura;
+
+    if (temperatura >= 28) {
+        icono.src = "./imgs/sunny.png";
+    } else if (temperatura >= 10) {
+        icono.src = "./imgs/fresh.png";
+    } else {
+        icono.src = "./imgs/cold.png";
+    }
+}
